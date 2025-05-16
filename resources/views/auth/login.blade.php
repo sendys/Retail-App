57a5b3c74ab9ba11c56183f2174a66561b9a5d8a
sendys <sandy.saryono@gmail.com>
2025-05-16 16:38:06 +0700

    add all assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,160 @@
-@extends('layouts.app')
+@include('layouts.partials.main')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    @include('layouts.partials.title-meta')
+    @include('layouts.partials.head-css')
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+</head>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<body class="authentication-bg authentication-bg-pattern">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <div class="account-pages mt-5 mb-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-4">
+                    <div class="card bg-pattern">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="card-body p-4">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                            <div class="text-center w-75 m-auto">
+                                <div class="auth-brand">
+                                    <a href="#" class="logo logo-dark text-center">
+                                        <span class="logo-lg">
+                                            <img src="{{ asset('assets/images/logo-dark.png') }}" alt=""
+                                                height="22">
+                                        </span>
                                     </a>
-                                @endif
+
+                                    <a href="index.php" class="logo logo-light text-center">
+                                        <span class="logo-lg">
+                                            <img src="{{ asset('assets/images/logo-light.png') }}" alt=""
+                                                height="22">
+                                        </span>
+                                    </a>
+                                </div>
+                                <p class="text-muted mb-4 mt-3">Enter your email address and password to access admin
+                                    panel.</p>
                             </div>
-                        </div>
-                    </form>
-                </div>
+
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+
+                                <div class="mb-3">
+                                    <label for="emailaddress" class="form-label">Email address</label>
+                                    <input class="form-control @error('email') is-invalid @enderror" type="email"
+                                        id="email" placeholder="Enter your email" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                    <span class="text-danger"></span>
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <div class="input-group input-group-merge">
+                                        <input type="password" id="password" name="password" required autocomplete="current-password" 
+                                            class="form-control @error('password') is-invalid @enderror"
+                                            placeholder="Enter your password">
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                        <div class="input-group-text" data-password="false">
+                                            <span class="password-eye"></span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="mb-3">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} >
+                                        <label class="form-check-label" for="checkbox-signin">Remember me</label>
+                                    </div>
+                                </div>
+
+                                <div class="text-center d-grid">
+                                    <button class="btn btn-primary" type="submit"> Log In </button>
+                                </div>
+
+                            </form>
+
+                            <div class="text-center">
+                                <h5 class="mt-3 text-muted">Sign in with</h5>
+                                <ul class="social-list list-inline mt-3 mb-0">
+                                    <li class="list-inline-item">
+                                        <a href="javascript: void(0);"
+                                            class="social-list-item border-primary text-primary"><i
+                                                class="mdi mdi-facebook"></i></a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="javascript: void(0);"
+                                            class="social-list-item border-danger text-danger"><i
+                                                class="mdi mdi-google"></i></a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="javascript: void(0);" class="social-list-item border-info text-info"><i
+                                                class="mdi mdi-twitter"></i></a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a href="javascript: void(0);"
+                                            class="social-list-item border-secondary text-secondary"><i
+                                                class="mdi mdi-github"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div> <!-- end card-body -->
+                    </div>
+                    <!-- end card -->
+
+                    <div class="row mt-3">
+                        <div class="col-12 text-center">
+                            <p> <a href="#" class="text-white-50 ms-1">Forgot your password?</a></p>
+                            <p class="text-white-50">Don't have an account? <a href="auth-register.php"
+                                    class="text-white ms-1"><b>Sign Up</b></a></p>
+                        </div> <!-- end col -->
+                    </div>
+                    <!-- end row -->
+
+                </div> <!-- end col -->
             </div>
+            <!-- end row -->
         </div>
+        <!-- end container -->
     </div>
-</div>
-@endsection
+    <!-- end page -->
+
+
+    <footer class="footer footer-alt">
+        <script>
+            document.write(new Date().getFullYear())
+        </script> &copy; Fintek Mutiara Indonesia
+    </footer>
+
+    <!-- Authentication js -->
+    <script src="{{ asset('assets/js/pages/authentication.init.js') }}"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const toggle = document.querySelector('.input-group-text[data-password]');
+            const passwordInput = document.getElementById('password');
+            const eye = toggle.querySelector('.password-eye');
+        
+            toggle.addEventListener('click', function () {
+                const isHidden = toggle.getAttribute('data-password') === 'false';
+                passwordInput.setAttribute('type', isHidden ? 'text' : 'password');
+                toggle.setAttribute('data-password', isHidden ? 'true' : 'false');
+        
+                // Ganti icon jika perlu (misalnya pakai CSS class atau innerHTML)
+                eye.classList.toggle('show'); // kamu bisa definisikan style berbeda untuk .show
+            });
+        });
+        </script>
+        
+</body>
+
+</html>
